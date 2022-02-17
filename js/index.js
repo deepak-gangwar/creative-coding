4038bd1740494a9623923dfdc3040b9423a4c397
Deepak <deepakgangwar4265@gmail.com>
2022-02-17 15:44:28 +0530

    squares rotating in circle and on their axis

--- js/index.js
+++ js/index.js
@@ -10,15 +10,49 @@ const params = {
 const two = new Two(params)
 two.appendTo(container)
 
-//creating the shape
-const shape = two.makeRectangle(250, 250, 100, 100)
-shape.fill = '#f9bc31'
-shape.noStroke()
-shape.rotation = Math.PI * 0.25
+//note: it would be cool to do this with Three.js
+//where along with this animation, cubes move on the planar axis as well
 
-//listening to any updates
+const numberOfShapes = 12
+const plotRadius = 150
+
+const shapes = [numberOfShapes]
+
+// creating the shape
+for(let i = 0; i < numberOfShapes; i = i + 1) {
+    const angle = fullRotation * i / numberOfShapes
+
+    // offsets
+    const x = plotRadius * Math.cos(angle)
+    const y = plotRadius * Math.sin(angle)
+    
+    // making the shapes
+    const shape = two.makeRectangle(x, y, 50, 50)
+    shape.noStroke()
+    shape.fill = '#f9bc31'
+    shape.rotation = angle
+
+    // adding them to a list so that I can use them 
+    // later on, outside of this scope
+    shapes.push(shape)
+}
+
+// making them a group so that i can rotate whole group at once
+const group = two.makeGroup(shapes)
+group.translation.set(250, 250)
+
+//listening to the updates
 two.bind("update", function () {
-    shape.rotation += 0.05
+    group.rotation += 0.005
+
+    //for(let i = 0; i < numberOfShapes; i++) {
+    //    shapes[i+1].rotation += 0.05
+    //}
+
+    shapes.forEach( shape => {
+        shape.rotation += 0.025
+    })
 })
 
 two.play()
+
