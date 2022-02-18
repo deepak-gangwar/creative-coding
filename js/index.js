da4e31436e233fa03a4a9832024854b04e590c22
Deepak <deepakgangwar4265@gmail.com>
2022-02-18 11:28:02 +0530

    coded upto creating those squares for project3

--- js/index.js
+++ js/index.js
@@ -1,24 +1,37 @@
-// Pick a tag to do our code in
-// then lets set up two.js in this tag
-const  container = document.querySelector("section")
+const container = document.querySelector("section")
 
-const params = {
-    width: 500,
-    height: 500
+const params = { 
+  width: 500, 
+  height: 500 
 }
 
 const two = new Two(params)
 two.appendTo(container)
 
-//creating the shape
-const shape = two.makeRectangle(250, 250, 100, 100)
-shape.fill = '#f9bc31'
-shape.noStroke()
-shape.rotation = Math.PI * 0.25
+// config for our animation
+const numberOfShapes = 40
+const shapeIncr = 20
+const shapes = []
 
-//listening to any updates
+// make shapes
+for (let i = 0; i < numberOfShapes; i++) { 
+  const size = (numberOfShapes - i) * shapeIncr
+  const shape = two.makeRectangle(250, 250, size, size)
+
+  if (i % 2 === 0) {
+    shape.fill = '#f9d2cd'
+  } else {
+    shape.fill = '#f55745'
+  }
+
+  shape.noStroke()
+
+  shapes.push(shape)
+}
+
 two.bind("update", function () {
-    shape.rotation += 0.05
+  // draw
 })
 
 two.play()
+
