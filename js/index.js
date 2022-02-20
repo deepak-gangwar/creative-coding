9fd5b57d280dfc400cf1684f06411359f05a64bf
Deepak <deepakgangwar4265@gmail.com>
2022-02-20 12:34:01 +0530

    Added easing and individual timelies, but goes one way

--- js/index.js
+++ js/index.js
@@ -16,13 +16,15 @@ const shapes = []
 const shapeMin = 0
 const shapeMax = 500
 const shapeDiff = shapeMax - shapeMin
+const loopDuration = 60 * 4
+const aDelay = 1 / 120
 
 // make shapes
 for (let i = 0; i < numberOfShapes; i++) { 
   const x = 250
   const y = 20 * i + 5
 
-  const shape = two.makeRectangle(x, y, shapeMax, 10)
+  const shape = two.makeRectangle(x, y, shapeMin, 10)
   shape.fill = '#5645d3'
   shape.noStroke()
 
@@ -30,8 +32,19 @@ for (let i = 0; i < numberOfShapes; i++) {
 }
 
 //listening to any updates
-two.bind("update", function () {
+two.bind("update", function (frameCount) {
   // draw
+  const currentFrame = frameCount % 240
+  const t = currentFrame / loopDuration
+
+  shapes.forEach((shape, i) => {
+    const aStart = 0.01 * (numberOfShapes - i)
+    const aEnd = 0.01 * i
+  
+    const u = mapAndClamp(t, aStart, 1 - aEnd, 0, 1)
+
+    shape.width = shapeMin + shapeDiff * easeInOutCubic(u)
+  })
 })
 
 two.play()
