335ecb8f1f353ef4fd2e75c323271890a8b6ee8b
Deepak <deepakgangwar4265@gmail.com>
2022-02-20 13:03:10 +0530

    Cleaning up the code

--- js/index.js
+++ js/index.js
@@ -42,33 +42,15 @@ two.bind("update", function (frameCount) {
   shapes.forEach((shape, i) => {
     const aStart = 0.01 * (numberOfShapes - i)
     const aEnd = 0.01 * i
-  
-    //const u = mapAndClamp(t, aStart, 1 - aEnd, 0, 1)
 
-    // Breaking the timeline into two parts
-    // So that it feels back and forth seamless
     let u = 0
-    
-    // We start at 0 go all the way upto 1
-    // and then back from 1 to 0 again
 
-    // mapAndClamp is breaking our timeline into parts
-
-    // if timeline is less than half completed
     if(t < 0.5) {
       u = mapAndClamp(t, aStart, 0.5 - aEnd, 0, 1)
-      // first two parameters mean start to half way through
-
-      // changes 1 to 0.5 in aEnd because doing it for half timeline
-      // keeping last two 0 and 1 because we want max shapeDiff
     } else {
       u = mapAndClamp(t, 0.5 +aStart, 1 - aEnd, 1, 0)
-      // first two params mean half way through to end
-
-      // added 0.5 (half timeline) to aStart
-      // ending again becomes 1 - aEnd
-      //reversed 0 and 1 in the end because now our initial point is 1
     }
+
     shape.width = shapeMin + shapeDiff * easeInOutCubic(u)
     shape.translation.x = 750 * easeInOutCubic(u)
   })
