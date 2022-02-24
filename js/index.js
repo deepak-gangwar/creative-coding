f66df239127e84fdb6b5d3cf560de477ac8e7d0a
Deepak <deepakgangwar4265@gmail.com>
2022-02-24 15:25:48 +0530

    Add initial shapes for moire

--- js/index.js
+++ js/index.js
@@ -13,10 +13,30 @@ two.appendTo(container)
 // config for our animation
 const numberOfShapes = 50
 const shapes = []
+const startWidth = 50
+const endWidth = 500
+const startRotation = 0
+const endRotation = fullRotation * 6 / 360
 
 // make shapes
 for (let i = 0; i < numberOfShapes; i++) { 
-  
+  const x = 250
+  let y = i * 20 + 5
+
+  if (i >= 25) {
+    y -= 490
+  }
+
+  const shape = two.makeRectangle(x, y, startWidth, 10)
+  shape.noStroke()
+  shape.fill = '#5645D3'
+
+  // We did the purple ones first and then did blue ones
+  // because we want blue ones to overlay the purple ones
+  if (i >= 25) {
+    shape.fill = '#99e6e0'
+  }
+  // We could have done modulo here, but we have tried to overlay
 }
 
 //listening to any updates
